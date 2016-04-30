76c10dcb51b619ce9ea41e7a188784c62a6a2749
Alex Gajewski <agajews@gmail.com>
2016-04-30 00:19:27 -0500

    Updated gitignore, stabilify

--- stabilify.sh
+++ stabilify.sh
@@ -1,2 +1,2 @@
-sed -i -e 's/[apricity-core-dev]/[apricity-core]/g' pacman/pacman.x86_64.conf
+sed -i -e 's/\[apricity-core-dev\]/\[apricity-core\]/g' pacman/pacman.x86_64.conf
 sed -i -e 's/apricity-core-dev/apricity-core-signed/g' pacman/pacman.x86_64.conf
