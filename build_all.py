89e4451951e5ef19f5382b82e5681faf716b543d
Alex Gajewski <agajews@gmail.com>
2016-04-29 22:09:30 -0500

    Switched to signed

--- build_all.py
+++
@@ -1,5 +0,0 @@
-from subprocess import call
-
-for edition in ['gnome', 'cinnamon']:
-    for arch in ['x84_64']:
-        call(['su -c "./build.sh -v -E ' + edition + '-A' + arch + '"'], shell=True)
