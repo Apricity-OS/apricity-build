74584d26d3b6ee60891faa9e5da3730240284054
Alex Gajewski <agajews@gmail.com>
2016-04-30 00:43:52 -0500

    Update stabilify.sh

--- stabilify.sh
+++ stabilify.sh
@@ -1,2 +1,4 @@
 sed -i -e 's/\[apricity-core-dev\]/\[apricity-core\]/g' pacman/pacman.x86_64.conf
 sed -i -e 's/apricity-core-dev/apricity-core-signed/g' pacman/pacman.x86_64.conf
+sed -i -e 's/\[apricity-core-dev\]/\[apricity-core\]/g' airootfs-all/etc/pacman.conf
+sed -i -e 's/apricity-core-dev/apricity-core-signed/g' airootfs-all/etc/pacman.conf
