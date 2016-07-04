d14819bf9d1da671ac3cd992f19cf5e057211d4c
Alex Gajewski <agajews@gmail.com>
2016-07-04 11:37:49 -0500

    Added buildpush

---
+++ buildpush.sh
@@ -0,0 +1,20 @@
+iso_name=apricity_os
+edition=gnome
+remove_prev=true
+verbose="-v"
+
+while getopts 'N:V:L:A:D:R:E:w:o:vh' arg; do
+    case "${arg}" in
+        N) iso_name="${OPTARG}" ;;
+        R) remove_prev="${OPTARG}" ;;
+        E) edition="${OPTARG}" ;;
+        v) verbose="-v" ;;
+        *)
+           echo "Invalid argument '${arg}'" ;;
+    esac
+done
+
+rm out/*
+./build.sh ${verbose} -E ${edition} -R ${remove_prev} -N ${iso_name}
+scp out/* apricity@apricityos.com:public_html/freezedry-build/${iso_name}.iso
+rm out/*
