89e8d4bdb7fb964b9d5c8aca1b9078a8d5e403e6
Alex Gajewski <agajews@gmail.com>
2016-07-13 12:31:23 -0500

    Added buildpush logs

--- buildpush.sh
+++ buildpush.sh
@@ -17,6 +17,7 @@ done
 rm out/*
 git checkout dev
 git pull origin dev
-./build.sh ${verbose} -E ${edition} -R ${remove_prev} -N ${iso_name}
-scp out/* apricity@apricityos.com:public_html/freezedry-build/${iso_name}.iso
+./build.sh ${verbose} -E ${edition} -R ${remove_prev} -N ${iso_name} > logs/${iso_name}.log
+scp out/* apricity@apricityos.com:public_html/freezedry-build/
+scp logs/${iso_name}.log apricity@apricityos.com:public_html/freezedry-build/
 rm out/*
