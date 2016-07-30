dac66a0eff44e340d8d3c85c2289048659390c90
Alex Gajewski <agajews@gmail.com>
2016-07-30 16:00:58 -0500

    Fixed upload

--- buildpush.sh
+++ buildpush.sh
@@ -23,6 +23,6 @@ sudo ./build.sh ${verbose} -E ${edition} -R ${remove_prev} -N ${iso_name} 2>&1 |
 sudo ssh server@192.241.147.116 "mkdir -p /mnt/static/public_html/freezedry-build/${username}"
 sudo scp out/* server@192.241.147.116:/mnt/static/public_html/freezedry-build/${username}/
 sudo scp logs/apricity_os-${iso_name}.log server@192.241.147.116:/mnt/static/public_html/freezedry-build/${username}/
-sudo ssh server@192.241.147.116 "rm /mnt/static/public_html/freezedry-build/${username}/apricity_os-${iso_name}.iso"
+# sudo ssh server@192.241.147.116 "rm /mnt/static/public_html/freezedry-build/${username}/apricity_os-${iso_name}.iso"
 sudo rm out/*
 sudo rm -rf work
